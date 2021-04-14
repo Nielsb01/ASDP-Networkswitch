5f375ae2143dd8ba90463ec55ba3936550253ef0
Wim Beukers <wimbeukers96@hotmail.nl>
2021-04-14 16:19:40 +0200

    Added EchoAll

--- NetworkSwitch/NetworkSwitch/Program.cs
+++ NetworkSwitch/NetworkSwitch/Program.cs
@@ -1,5 +1,4 @@
-﻿
-using WebSocketSharp;
+﻿using WebSocketSharp;
 using WebSocketSharp.Server;
 using System;
 
@@ -28,12 +27,35 @@ namespace NetworkSwitch
         }
     }
 
+    public class EchoAll : WebSocketBehavior
+    {
+        protected override void OnOpen()
+        {
+            Console.WriteLine("connection opened");
+            base.OnOpen();
+        }
+
+        protected override void OnMessage(MessageEventArgs e)
+        {
+            Sessions.Broadcast(e.Data);
+            Console.WriteLine($"received: {e.Data}");
+            base.OnMessage(e);
+        }
+
+        protected override void OnClose(CloseEventArgs e)
+        {
+            Console.WriteLine("connection closed");
+            base.OnClose(e);
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
             WebSocketServer wss = new WebSocketServer("ws://localhost:8088");
             wss.AddWebSocketService<Echo>("/Echo");
+            wss.AddWebSocketService<EchoAll>("/EchoAll");
 
             wss.Start();
             Console.WriteLine("started on port 8088");
