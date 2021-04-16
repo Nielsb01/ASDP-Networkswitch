0e15f54e66746be32f020ba2575c3a02deaf2fef
L.tenHaaf1@student.han.nl <L.tenHaaf1@student.han.nl>
2021-04-16 10:37:12 +0200

    Duplicated project to new folder so prototype with rooms will still be available. - Removed redundant code in original prototype.

--- websocketserver-prototype-nodejs/index.js
+++ websocketserver-prototype-nodejs/index.js
@@ -8,30 +8,14 @@ const io = require('socket.io')(server,{
 });
 
 io.on('connection', (socket) => {
-    console.log("New connection with id:" + socket.id)
-    socket.join('count');
+    console.log("New client with id: " + socket.id)
 
     socket.onAny((eventName, msg) => {
-        console.log(`Received event '${eventName}' with payload '${msg}'`);
-
-        const roomName = socket.data.roomName;
-
-        if (roomName != null) {
-            socket.to(roomName).emit(eventName, msg);
-        } else {
-            console.log("Could not broadcast message because client is not in a room!");
-        }
-    })
-
-    socket.on('join-room', (roomName) => {
-        console.log(`Client: ${socket.id} will join ${roomName}`)
-        socket.join(roomName)
-        socket.data.roomName = roomName;
-    })
+        console.log(`Broadcasting received event '${eventName}' with payload '${msg}' from '${socket.id}'.`);
+        socket.broadcast.emit(eventName, msg);
+    });
 
     socket.on("disconnect", (reason) => {
-        console.log(`Client disconnected with reason ${reason}`);
+        console.log(`Client '${socket.id}' disconnected with reason ${reason}`);
     });
 });
-
-// io.of("/").adapter.rooms.get('count')?.size
