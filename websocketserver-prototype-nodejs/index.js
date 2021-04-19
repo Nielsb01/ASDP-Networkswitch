2c8210039b3706b092c0b66b1cde4552c1b4bb4e
Jordi Stevens <JSA.Stevens@student.han.nl>
2021-04-19 09:37:41 +0200

    Able to make a docker image

--- websocketserver-prototype-nodejs/index.js
+++ websocketserver-prototype-nodejs/index.js
@@ -1,6 +1,13 @@
 const express = require('express');
 const app = express();
-const server = app.listen(process.env.PORT | 3000);
+const port = process.env.PORT | 8080;
+
+app.get('/', (req, res) => {
+    res.send('Hello World');
+});
+
+const server = app.listen(port);
+console.log(`Running on http://0.0.0.0:${port}`);
 const io = require('socket.io')(server,{
     cors: {
         origin: "*",
@@ -19,3 +26,4 @@ io.on('connection', (socket) => {
         console.log(`Client '${socket.id}' disconnected with reason ${reason}`);
     });
 });
+
