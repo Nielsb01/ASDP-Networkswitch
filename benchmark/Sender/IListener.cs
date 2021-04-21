88514d0dbe6fda3468add9dec1f83ff5938c2ebb
Luuk ten Haaf <L.tenHaaf1@student.han.nl>
2021-04-21 14:01:32 +0200

    IListener.cs edited online with Bitbucket

--- benchmark/Sender/IListener.cs
+++ benchmark/Sender/IListener.cs
@@ -1,11 +1,3 @@
-//     AIM SD ASD 2020/2021 S2 project
-// 
-//     Project name: [to be determined]
-// 
-//     This file is created by group 5 [Luuk, Niels, Pepijn, Wim, Jordi]
-// 
-//     Goal of this file: [making_the_system_work]
-
 namespace Sender
 {
     public interface IListener
